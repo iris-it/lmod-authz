9b0232922bb8b4986c6f616666d2679aa7ee197d
ci_trex <alexandre.mangin@viacesi.fr>
2018-01-02 13:18:03 +0100

    fix pill size + wrong variable on permissions

--- resources/views/codebase/admin/permissions/forms/roles_sync.blade.php
+++ resources/views/codebase/admin/permissions/forms/roles_sync.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group row {{ ($errors->has('roles[]')) ? 'is-invalid' : '' }}">
     <div class="col-12">
         <div class="form-material floating">
-            {!! Form::select('roles[]', $permissions, array_pluck($permission->roles, 'id'), ['multiple', 'class'=> 'form-control'] ) !!}
+            {!! Form::select('roles[]', $roles, array_pluck($permission->roles, 'id'), ['multiple', 'class'=> 'form-control'] ) !!}
             {!! Form::label('roles[]', __('Roles')) !!}
         </div>
         @if ($errors->has('roles[]'))
